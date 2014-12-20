17d922225363e2cdfba100a893fedcd968553719
mwech <mwech@student.tgm.ac.at>
2014-12-20 13:27:33 +0100

    Minor changes at the client class

--- patterns/decorator/client.py
+++ patterns/decorator/client.py
@@ -1,10 +1,15 @@
+from patterns.decorator.Fisch import Fisch
 from patterns.decorator.Pommes import Pommes
+from patterns.decorator.Salat import Salat
 from patterns.decorator.Steak import Steak
 from patterns.decorator.WienerSchnitzel import WienerSchnitzel
 __author__ = 'mwech'
 
 schnitzel = WienerSchnitzel()
-print(schnitzel.getBezeichnung() + ": $" + str(schnitzel.getPreis()))
+print(schnitzel.getBezeichnung() + ": €" + str(schnitzel.getPreis()))
 
 steak = Pommes(Steak())
-print(steak.getBezeichnung() + ": $" + str(steak.getPreis()))
+print(steak.getBezeichnung() + ": €" + str(steak.getPreis()))
+
+fisch = Salat(Pommes(Fisch()))
+print(fisch.getBezeichnung() + ": €" + str(fisch.getPreis()))
